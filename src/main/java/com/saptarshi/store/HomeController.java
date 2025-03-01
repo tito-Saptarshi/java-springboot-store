dcad37268cda85b99d3648528fd61b8a6a761836
tito-Saptarshi <saptarshi.tito.20@gmail.com>
2025-03-01 21:54:34 +0530

    general -u 01032025

--- src/main/java/com/saptarshi/store/HomeController.java
+++ src/main/java/com/saptarshi/store/HomeController.java
@@ -1,7 +1,13 @@
 package com.saptarshi.store;
 
 import org.springframework.stereotype.Controller;
-// check commit-2
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// web controller for resaving web traffic
 @Controller
 public class HomeController {
+    @RequestMapping("/")  // if any req goes to this page, the below function gets called / getting traffic to the provided url
+    public String index() {
+        return "index.html";
+    }
 }
